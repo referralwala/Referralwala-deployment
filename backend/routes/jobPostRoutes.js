59533b92d6b54198bf66f61e61029e1d4592ce7d
Sushant Kumar <115036214+sushant010@users.noreply.github.com>
2024-10-26 02:52:55 +0530

    addded jwt middleware back

--- backend/routes/jobPostRoutes.js
+++ backend/routes/jobPostRoutes.js
@@ -21,31 +21,31 @@ const router = express.Router();
 router.post('/create',jwtMiddleware, createJobPost);
 
 // Get all job posts
-router.get('/all',getAllJobPosts);
+router.get('/all',jwtMiddleware,getAllJobPosts);
 
 // Get a job post by ID
-router.get('/:id', getJobPostById);
+router.get('/:id', jwtMiddleware,getJobPostById);
 
 // Apply for a job post
-router.post('/apply/:id', applyForJobPost);
+router.post('/apply/:id',jwtMiddleware, applyForJobPost);
 
 // Get applicants for a job post
-router.get('/applicants/:id',getApplicantsForJobPost);
+router.get('/applicants/:id',jwtMiddleware,getApplicantsForJobPost);
 
 //Get Applicants status on all jobs
-router.get('/user/:userId/applications/statuses', getUserApplicationStatuses);
+router.get('/user/:userId/applications/statuses', jwtMiddleware,getUserApplicationStatuses);
 
 //Get Applicant status on specific job post
-router.get('/user/:userId/jobpost/:jobPostId/application/status', getApplicationStatusForJobPost);
+router.get('/user/:userId/jobpost/:jobPostId/application/status', jwtMiddleware,getApplicationStatusForJobPost);
 
 // Update applicant status
-router.put('/:jobId/applicant/:applicantId/status',updateApplicantStatus);
+router.put('/:jobId/applicant/:applicantId/status',jwtMiddleware,updateApplicantStatus);
 
 // Update a job post
-router.put('/update/:id', updateJobPost);
+router.put('/update/:id',jwtMiddleware, updateJobPost);
 
 // get job by uniqueid
-router.get('/unique/:jobUniqueId', getJobsByJobUniqueId);
+router.get('/unique/:jobUniqueId',jwtMiddleware, getJobsByJobUniqueId);
 
 // Delete a job post
 router.delete('/delete/:id',jwtMiddleware, deleteJobPost);
